a1f2c288efae558b754fe6b490829c6c4e26d128
nefillanes <lla210001@byui.edu>
2025-01-07 19:13:22 -0600

    Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+const year = today.getFullYear();
+
+currentyear.innerHTML = year;
+lastModified.innerHTML = `Last Modification: <span>${document.lastModified}</span>`;
