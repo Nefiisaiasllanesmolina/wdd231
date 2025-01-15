bc1738aa10209eca94c6751c1b306f69d95e354d
nefillanes <lla210001@byui.edu>
2025-01-14 19:17:44 -0600

    Home Page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,16 @@ const year = today.getFullYear();
 
 currentyear.innerHTML = year;
 lastModified.innerHTML = `Last Modification: <span>${document.lastModified}</span>`;
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
